296a6a1dc495fbec6b755d4020f0e70cb0b026b4
Kajal Sanwal <kajalsanwal777@gmail.com>
2026-01-26 17:29:06 +0530

    login and signup completed

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,23 +1,48 @@
 "use client";
 import Link from 'next/link';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
-import { Axios } from 'axios';
+import axios, { Axios } from 'axios';
+import toast from 'react-hot-toast';
 
 
 export default function SignupPage (){
+    const router = useRouter();
     const [user, setUser] = React.useState({  //grabbing user info
         email: "",
         password: "",
         username: ""
     })
+    const [buttonDisabled, setButtonDisabled] = React.useState(false);
+    const [loading, setLoading] = React.useState(false);
 
-    const onSignup = async () => {    // after signup
+    useEffect(()=>{
+           if(user.email.length > 0 && user.password.length > 0 
+            && user.username.length > 0){
+            setButtonDisabled(false);
+           }else{
+            setButtonDisabled(true);
+           }
+    }, [user]);  
 
-    }
+    const onSignup = async () => { // after signup  
+        try {
+            setLoading(true);
+            const response = await axios.post("/api/users/signup", user);
+            console.log("Signup success", response.data);
+            router.push("/login");
+            
+        } catch (error:any) {
+            console.log("Signup failed!", error.message);
+
+            toast.error(error.message);
+        }finally{
+            setLoading(false);
+        }
+     }  
     return(
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
-            <h1>Sign up</h1>
+            <h1>{loading ? "Processing..." : "Signup!"}</h1>
             <hr></hr>
             <label htmlFor='username'>username</label>
             <input 
@@ -51,8 +76,8 @@ export default function SignupPage (){
 
             <button 
             onClick={onSignup}
-            className='p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600'
-            >Submit!</button>
+            className='p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600'>
+                {buttonDisabled ? "Duh, fillup!" : "Signup!"}</button>
             <Link href="/login">Visit login page!</Link>
 
 
